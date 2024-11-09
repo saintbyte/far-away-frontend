b37cd3c1b2b1631ba22d12b3fdcbfd448df06f2d
Yegor Kazantsev <wwwadmin@bk.ru>
2024-11-09 03:21:52 +0300

    css

--- editor.js
+++ editor.js
@@ -4,6 +4,23 @@ import Quill from 'quill';
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.bubble.css'
 import 'quill/dist/quill.snow.css'
+import i18next from 'i18next';
+
+function init() {
+    i18next.init({
+        lng: 'en', // if you're using a language detector, do not define the lng option
+        debug: true,
+        resources: {
+          en: {
+            translation: {
+              "Title": "Title",
+              "Author": "author",
+              "TextPlaceholder": "Text..."
+            }
+          }
+        }
+      });
+}
 
 function setupQuill(element) {
     const quill = new Quill(
@@ -11,23 +28,25 @@ function setupQuill(element) {
         {
             debug: 'info',
             theme: 'bubble',
-            placeholder: 'Text...',
+            placeholder: i18next.t("TextPlaceholder"),
         }
     );
     return quill
 }
   
-
+init()
 document.querySelector('#app').innerHTML = `
-  <div>
-    <div id="editor"></div>
-    <p class="read-the-docs" >
-      Click on the Vite logo to learn more
-    </p>
-    <p id="counter"></p>
-  </div>
-
+<div>
+    <div class="article-header">
+        <div class="article-header-title">
+            <input type="text" id="title" placeholder="${i18next.t("Title")}">
+        </div>
+        <div class="article-header-author">
+            <input type="text" id="author" placeholder="${i18next.t("Author")}">
+        </div>
+    </div> 
+</div>
+<div id="editor"></div>
+</div>
 `
-
-const editor = setupQuill(document.querySelector('#editor'))
-setupCounter(document.querySelector('#counter'))
+const editor = setupQuill(document.querySelector('#editor'));
